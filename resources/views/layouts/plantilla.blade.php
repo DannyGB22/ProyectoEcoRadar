c0f90f4cedec0a3868076a908466b105c0c1f7aa
Danny22 <125704277+DannyGB22@users.noreply.github.com>
2024-06-04 08:49:10 -0600

    Instalacion de tailwind

--- resources/views/layouts/plantilla.blade.php
+++ resources/views/layouts/plantilla.blade.php
@@ -7,6 +7,7 @@
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <title>@yield('titulo')</title>
     @vite('resources/js/app.js')
+    @vite('resources/css/app.css')
     <link href="https://fonts.googleapis.com/css2?family=Alice&display=swap" rel="stylesheet">
 
 </head>
